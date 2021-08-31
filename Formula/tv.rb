2f92b31977c0c5c23e43ec5747abbe8416094d20
uzimaru0000 <uzimaru0000@users.noreply.github.com>
2021-08-31 20:47:07 +0000

    Update packages

--- Formula/tv.rb
+++ Formula/tv.rb
@@ -1,9 +1,9 @@
 class Tv < Formula
-  desc "Format json and csv into table view"
+  desc "Format"
   homepage "https://github.com/uzimaru0000/tv"
-  url "https://github.com/uzimaru0000/tv/releases/download/v0.2.0/tv-x86_64-apple-darwin.zip"
-  sha256 "94e80e64a07e3071bb119c77b13fccb776fd9d27819a8ba7dabdc54f3289ab87"
-  version "0.2.0"
+  url "json"
+  sha256 "into"
+  version "table"
 
   def install
     bin.install "tv"
